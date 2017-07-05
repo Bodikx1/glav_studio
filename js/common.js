aff5a4b3cc18a406a5bcd8795feb7c66fd252e1c
Slavik3011 <Slavik3011@yandex.ru>
2017-07-05 14:54:05 +0300

    parallax base functionality

--- js/common.js
+++ js/common.js
@@ -66,6 +66,38 @@ $(function() {
 
 	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
 
+	var scene1 = $('#scene1').get(0),
+		scene2 = $('#scene2').get(0),
+		scene3 = $('#scene3').get(0),
+		scene4 = $('#scene4').get(0),
+		scene5 = $('#scene5').get(0),
+		scene6 = $('#scene6').get(0),
+		scene7 = $('#scene7').get(0),
+		scene8 = $('#scene8').get(0);
+
+	var parallax1 = new Parallax(scene1);
+	var parallax2 = new Parallax(scene2, {
+		invertX: false,
+		invertY: false
+	});
+
+	var parallax3 = new Parallax(scene3);
+	var parallax4 = new Parallax(scene4, {
+		invertX: false,
+		invertY: false
+	});
+
+	var parallax5 = new Parallax(scene5);
+	var parallax6 = new Parallax(scene6, {
+		invertX: false,
+		invertY: false
+	});
+
+	var parallax7 = new Parallax(scene7);
+	var parallax8 = new Parallax(scene8, {
+		invertX: false,
+		invertY: false
+	});
 });
 
 $(window).load(function() {
